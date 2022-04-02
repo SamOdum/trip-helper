81888f7bcb441e71d1f50eda1c054c3c84bc701c
Sam Odum <sam.odum1@gmail.com>
2022-04-02 11:56:18 +0200

    Initialise base App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,54 @@
-import React from "react";
+import { useState } from "react";
+import { CssBaseline, Grid } from "@mui/material";
+import Header from "./components/Header/Header";
+import List from "./components/List/List";
+import Map from "./components/Map/Map";
+import { Bounds, Coords } from "google-map-react";
+import { PlacesDataProps } from "./api/types";
+import { useGetPlaces, useUserLocation } from "./hooks";
 
 const App = () => {
+  const initialCoords = {
+    lat: 0,
+    lng: 0,
+  };
+
+  const [places, setPlaces] = useState<PlacesDataProps>([]);
+  const [coordinates, setCoordinates] = useState<Coords>(initialCoords);
+  const [bounds, setBounds] = useState<Pick<Bounds, "ne" | "sw">>({
+    ne: initialCoords,
+    sw: initialCoords,
+  });
+
+  useUserLocation(setCoordinates);
+  useGetPlaces(bounds, setPlaces);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello world</h1>
-      </header>
-    </div>
+    <>
+      <CssBaseline />
+      <Header />
+      <Grid
+        container
+        spacing={0}
+        style={{
+          width: "100%",
+          // paddingTop: "2rem",
+          // height: "89vh",
+          // overflowY: "clip",
+        }}
+      >
+        <Grid item xs={12} md={4}>
+          <List places={places} />
+        </Grid>
+        <Grid item xs={12} md={8}>
+          <Map
+            coordinates={coordinates}
+            setCoordinates={setCoordinates}
+            setBounds={setBounds}
+          />
+        </Grid>
+      </Grid>
+    </>
   );
 };
 
