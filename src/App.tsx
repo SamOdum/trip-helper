26826155e9947dbc535574c44ff7a2536ec770a5
Sam Odum <sam.odum1@gmail.com>
2022-04-04 09:17:26 +0200

    Refine App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,56 @@
-import { useState } from "react";
 import { CssBaseline, Grid } from "@mui/material";
 import Header from "./components/Header/Header";
 import List from "./components/List/List";
 import Map from "./components/Map/Map";
-import { Bounds, Coords } from "google-map-react";
-import { PlacesDataProps } from "./api/types";
-import { useGetPlaces, useUserLocation } from "./hooks";
+import { useAutoComplete, useGetPlaces, useUserLocation } from "./hooks";
 
 const App = () => {
-  const initialCoords = {
-    lat: 0,
-    lng: 0,
-  };
-
-  const [places, setPlaces] = useState<PlacesDataProps>([]);
-  const [coordinates, setCoordinates] = useState<Coords>(initialCoords);
-  const [bounds, setBounds] = useState<Pick<Bounds, "ne" | "sw">>({
-    ne: initialCoords,
-    sw: initialCoords,
-  });
-
-  useUserLocation(setCoordinates);
-  useGetPlaces(bounds, setPlaces);
+  const { coordinates, setCoordinates } = useUserLocation();
+  const { onLoad, onPlaceChanged } = useAutoComplete(setCoordinates);
+  const {
+    type,
+    places,
+    rating,
+    loading,
+    childClicked,
+    filteredPlaces,
+    setType,
+    setRating,
+    setBounds,
+    setChildClicked,
+  } = useGetPlaces();
 
   return (
     <>
       <CssBaseline />
-      <Header />
+      <Header onLoad={onLoad} onPlaceChanged={onPlaceChanged} />
       <Grid
         container
         spacing={0}
         style={{
           width: "100%",
-          // paddingTop: "2rem",
-          // height: "89vh",
-          // overflowY: "clip",
+          paddingTop: "2rem",
+          height: "89vh",
         }}
       >
         <Grid item xs={12} md={4}>
-          <List places={places} />
+          <List
+            places={filteredPlaces.length > 0 ? filteredPlaces : places}
+            childClicked={childClicked}
+            loading={loading}
+            type={type}
+            setType={setType}
+            rating={rating}
+            setRating={setRating}
+          />
         </Grid>
         <Grid item xs={12} md={8}>
           <Map
             coordinates={coordinates}
             setCoordinates={setCoordinates}
             setBounds={setBounds}
-            places={places}
+            places={filteredPlaces.length > 0 ? filteredPlaces : places}
+            setChildClicked={setChildClicked}
           />
         </Grid>
       </Grid>
