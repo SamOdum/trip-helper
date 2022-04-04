63ab4e2b6308a1d5b9dc54a36a39293815212636
Sam Odum <sam.odum1@gmail.com>
2022-04-04 09:34:41 +0200

    Update hooks and places fallback img

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,30 +1,137 @@
-import { Bounds } from "google-map-react";
-import { useEffect } from "react";
+import { Bounds, Coords } from "google-map-react";
+import {
+  createRef,
+  Dispatch,
+  RefObject,
+  SetStateAction,
+  useEffect,
+  useState,
+} from "react";
 import { getPlacesData } from "../api";
 import { PlacesDataProps } from "../api/types";
 
-type LocationCoordProps = {
-  lat: number;
-  lng: number;
+const initialCoords = {
+  lat: 0,
+  lng: 0,
 };
 
-const useUserLocation = (fn: ({ lat, lng }: LocationCoordProps) => void) => {
+const useUserLocation = () => {
+  const [loading, setLoading] = useState<boolean>(false);
+  const [coordinates, setCoordinates] = useState<Coords>(initialCoords);
+
   useEffect(() => {
+    setLoading(true);
     navigator.geolocation.getCurrentPosition(
-      ({ coords: { latitude, longitude } }) =>
-        fn({ lat: latitude, lng: longitude })
+      ({ coords: { latitude, longitude } }) => {
+        setCoordinates({ lat: latitude, lng: longitude });
+        setLoading(false);
+      }
     );
-  }, [fn]);
+  }, [setCoordinates]);
+
+  return { coordinates, loading, setCoordinates };
 };
 
-const useGetPlaces = (
-  bounds: Pick<Bounds, "ne" | "sw">,
-  fn: (data: PlacesDataProps) => void
-) => {
+const useGetClickedChild = () => {
+  const [childClicked, setChildClicked] = useState<number>();
+
+  return { childClicked, setChildClicked };
+};
+
+const useGetPlaces = () => {
+  const [rating, setRating] = useState<number>(0);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [type, setType] = useState<string>("restaurants");
+  const [places, setPlaces] = useState<PlacesDataProps>([]);
+  const [childClicked, setChildClicked] = useState<number>();
+  const [filteredPlaces, setFilteredPlaces] = useState<PlacesDataProps>([]);
+  const [bounds, setBounds] = useState<Pick<Bounds, "ne" | "sw">>({
+    ne: initialCoords,
+    sw: initialCoords,
+  });
+  console.log({ rating });
+
   useEffect(() => {
-    const apiPlaces = getPlacesData({ ne: bounds.ne, sw: bounds.sw });
-    apiPlaces.then((data) => fn(data));
-  }, [bounds, fn]);
+    const arrayOfFiltered = places.filter(
+      (place) => Number(place.rating) > rating
+    );
+    setFilteredPlaces(arrayOfFiltered);
+    setChildClicked(0);
+    console.log("setFilteredPlaces called!");
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [rating]);
+
+  useEffect(() => {
+    setLoading(true);
+    const apiPlaces = getPlacesData({
+      ne: bounds.ne,
+      sw: bounds.sw,
+      type,
+    });
+    console.log("getPlacesData called!");
+
+    apiPlaces.then((data) => {
+      setPlaces(data);
+      setFilteredPlaces([]);
+      setRating(0);
+      setChildClicked(0);
+      setLoading(false);
+    });
+  }, [bounds, setPlaces, setLoading, type]);
+
+  return {
+    type,
+    places,
+    rating,
+    loading,
+    childClicked,
+    filteredPlaces,
+    setType,
+    setRating,
+    setBounds,
+    setChildClicked,
+  };
 };
 
-export { useUserLocation, useGetPlaces };
+const useElementRefs = (arrOfInterest: {}[]) => {
+  const [elementRefs, setElementRefs] = useState<RefObject<HTMLDivElement>[]>(
+    []
+  );
+
+  useEffect(() => {
+    const refs = Array(arrOfInterest?.length)
+      .fill(createRef<HTMLDivElement>())
+      .map((_, i) => elementRefs[i] || createRef<HTMLDivElement>());
+    setElementRefs(refs);
+
+    // DO NOT include elementRefs in the dependency array to
+    // prevent an infinite render loop
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [arrOfInterest]);
+
+  return elementRefs;
+};
+
+const useAutoComplete = (coordFn: Dispatch<SetStateAction<Coords>>) => {
+  const [autocomplete, setAutocomplete] =
+    useState<google.maps.places.Autocomplete>();
+
+  const onLoad = (autoC: google.maps.places.Autocomplete) =>
+    setAutocomplete(autoC);
+  const onPlaceChanged = () => {
+    const lat = autocomplete?.getPlace()?.geometry?.location?.lat() || 0;
+    const lng = autocomplete?.getPlace()?.geometry?.location?.lng() || 0;
+    coordFn({ lat, lng });
+  };
+
+  return { onLoad, onPlaceChanged };
+};
+
+export {
+  useAutoComplete,
+  useElementRefs,
+  useGetClickedChild,
+  useGetPlaces,
+  useUserLocation,
+};
