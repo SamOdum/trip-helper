6e8df7ea7746abb3a1c9a09b5847905e08f681a6
Sam Odum <sam.odum1@gmail.com>
2022-04-05 21:35:21 +0200

    Clean-up details component

--- src/components/PlaceDetails/PlaceDetails.tsx
+++ src/components/PlaceDetails/PlaceDetails.tsx
@@ -52,26 +52,6 @@ const PlaceDetails = ({ place, selected, refProp }: PlaceDetailsProps) => {
             {place.price}
           </Typography>
         </Box>
-        <Box display="flex" justifyContent="space-between">
-          <Typography variant="subtitle1">Ranking </Typography>
-          <Typography gutterBottom variant="subtitle1">
-            {place.ranking}
-          </Typography>
-        </Box>
-        {place?.awards?.map((award) => (
-          <Box
-            key={award.display_name}
-            display="flex"
-            justifyContent="space-between"
-            alignItems="center"
-            my={1}
-          >
-            <img src={award.images.small} alt={award.display_name} />
-            <Typography variant="subtitle2" color="textSecondary">
-              {award.display_name}
-            </Typography>
-          </Box>
-        ))}
         {place?.cuisine?.map(({ name }) => (
           <Chip key={name} label={name} className={classes.chip} />
         ))}
